48880b7ffc75ddba08b259802eec0ed6ae50a2f5
euijeong-Kang <dmlwjd558@gmail.com>
2022-09-15 18:29:19 +0900

    패키지 구조 변경

--- src/main/java/com/betheve/betheve/config/WebConfig.java
+++ src/main/java/com/betheve/betheve/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.betheve.betheve.common.config;
+package com.betheve.betheve.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

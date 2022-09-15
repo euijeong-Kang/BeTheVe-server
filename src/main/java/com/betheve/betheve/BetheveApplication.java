e28b7f679793df49d4270eccf64a8886a6253816
euijeong-Kang <dmlwjd558@gmail.com>
2022-09-15 18:03:17 +0900

    first commit

---
+++ src/main/java/com/betheve/betheve/BetheveApplication.java
@@ -0,0 +1,13 @@
+package com.betheve.betheve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetheveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetheveApplication.class, args);
+    }
+
+}

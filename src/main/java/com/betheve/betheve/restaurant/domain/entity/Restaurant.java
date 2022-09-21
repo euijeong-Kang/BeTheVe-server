3e0eac12c6e918a817308f197dec305e969affbb
euijeong-Kang <dmlwjd558@gmail.com>
2022-09-21 17:46:29 +0900

    Restaurant Entity 추가

---
+++ src/main/java/com/betheve/betheve/restaurant/domain/entity/Restaurant.java
@@ -0,0 +1,40 @@
+package com.betheve.betheve.restaurant.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@Table(name = "Restaurant")
+@AllArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(generator = "auto")
+    private Long restaurantId;
+
+    private String restaurantName;
+
+    private String PhoneNum;
+
+    @Embedded
+    private BusinessHour openingHours;
+
+    @Embedded
+    private BusinessHour breakTime;
+
+    private boolean hasParking;
+
+    private LocalDateTime resisterDate;
+
+    private LocalDateTime lastUpdatedDate;
+
+
+    public Restaurant() {}
+
+}
